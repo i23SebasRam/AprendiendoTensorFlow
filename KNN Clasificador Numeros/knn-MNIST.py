183d308b6e2bc8dc3e172057f7580d6d836976d4
i23SebasRam <80076520+i23SebasRam@users.noreply.github.com>
2021-05-08 16:38:36 -0500

    Se creo la funcion que junta dos imagenes y sus labels, ademas que creo un nuevo label que sucede cuando los dos numeros son totalmente iguales

--- KNN Clasificador Numeros/knn-MNIST.py
+++ KNN Clasificador Numeros/knn-MNIST.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import fetch_openml
 import matplotlib.pyplot as plt 
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import SGDClassifier
 import cv2
 
@@ -10,6 +11,12 @@ x,y = mnist['data'],mnist['target']
 
 xlist = x.values
 y = y.astype(np.uint8)
+ylist = y.values 
+ylist = ylist.reshape(-1,1)
+
+datos = np.append(xlist,ylist,axis = 1)
+datospd = pd.DataFrame(data=datos)
+datospd.to_csv("D:/AprendiendoTensorFlow/KNN Clasificador Numeros/datosMNIST.csv")
 
 digito = xlist[0,:]
 digito = digito.reshape(28,28)
@@ -62,14 +69,57 @@ knn_clf.predict(imgBW)
 total = np.zeros((28,56))
 izquierda = x.values[0].reshape(28,28)
 derecha = x.values[1].reshape(28,28)
-derecha = np.roll(derecha,-2)
+#derecha = np.roll(derecha,-2)
 total = np.concatenate((izquierda,derecha),axis=1)
+total1 = total.reshape(1,-1)
+total2 = total1.reshape(28,56)
 
-plt.imshow(total,cmap="binary")
+plt.imshow(total2,cmap="binary")
 plt.axis("off")
 plt.show()
 
 label = np.zeros((9))
 label[y[0]] = 1
 label[y[1]] = 1
+
+## Quiero hacer una funcion que me expanda los datos para entrenar el modelo, con datos de dos cifras
+
+def hagoDosCifras(xlist,ylist,vecesIzquierda,porcen):
+    Hx = xlist.shape
+    #No necesitamos la de y porque son las mismas dimensiones
+    cantDatos = int(Hx*porcen)
+    xlist = xlist[:cantDatos]
+    #Me deberia de tirar la fila desde cero hasta la cantidad de datos que queremos.
+    ylist = ylist[:cantDatos]
+    #Me deberia de tirar los labels de la misma cantidad.
+    xNuevo = np.zeros((cantDatos*vecesIzquierda,28*56),dtype=np.uint8)
+    yNuevo = np.zeros((cantDatos*vecesIzquierda,11),dtype=np.uint8)
+    
+    #La ultima columna de yNuevo es cuando los dos numeros son iguales.
+    for i in range(vecesIzquierda):
+        numIzquierda = xlist[i].reshape(28,28)
+        for j in range(cantDatos):
+        #Aca tomo el numero de la izquierda
+            numDerecha = xlist[j].reshape(28,28)
+            xSemiNuevo = np.concatenate((numIzquierda,numDerecha),axis=1)
+            #xSemiNuevo[28,56]
+            xNuevo[j+cantDatos*i] = xSemiNuevo.reshape(1,-1)
+            if ylist[i] == ylist[j]:
+                #Si son el mismo numero.
+                yNuevo[j+cantDatos*i,ylist[i]] = 1
+                yNuevo[j+cantDatos*i,10] = 1
+            else:
+                yNuevo[j+cantDatos*i,ylist[i]] = 1
+                yNuevo[j+cantDatos*i,ylist[j]] = 1
+    return xNuevo,yNuevo
+
+pruebaX,pruebaY = hagoDosCifras(xlist,ylist,5,0.001)
+
+prueba = pruebaX[348].reshape(28,56)
+print(pruebaY[348])
+plt.imshow(prueba,cmap="binary")
+plt.axis("off")
+plt.show()
+
+pruebaX,pruebaY = hagoDosCifras(xlist,ylist,20,0.4)
 
