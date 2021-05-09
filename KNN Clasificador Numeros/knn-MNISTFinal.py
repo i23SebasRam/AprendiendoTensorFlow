a120525fe638b65972489ee51b380d245ab22e73
i23SebasRam <80076520+i23SebasRam@users.noreply.github.com>
2021-05-09 11:17:53 -0500

    Se probaron los modelos, se escogio uno, y ademas se probo en video, no funciono

--- KNN Clasificador Numeros/knn-MNISTFinal.py
+++ KNN Clasificador Numeros/knn-MNISTFinal.py
@@ -1,9 +1,13 @@
 from sklearn.datasets import fetch_openml
 import matplotlib.pyplot as plt 
 import numpy as np
-import pandas as pd
-from sklearn.linear_model import SGDClassifier
+#import pandas as pd
+#from sklearn.linear_model import SGDClassifier
 import cv2
+
+path = "C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robotica/prueba.jpg"
+img = cv2.imread(path)
+
 
 mnist = fetch_openml('mnist_784', version=1)
 x,y = mnist['data'],mnist['target']
@@ -14,7 +18,7 @@ ylist = y.values
 ylist = ylist.reshape(-1,1)
 
 def hagoDosCifras(xlist,ylist,vecesIzquierda,porcen):
-    Hx = xlist.shape
+    Hx,Wx = xlist.shape
     #No necesitamos la de y porque son las mismas dimensiones
     cantDatos = int(Hx*porcen)
     xlist = xlist[:cantDatos]
@@ -42,12 +46,99 @@ def hagoDosCifras(xlist,ylist,vecesIzquierda,porcen):
                 yNuevo[j+cantDatos*i,ylist[j]] = 1
     return xNuevo,yNuevo
 
-x2,y2 = hagoDosCifras(xlist,ylist,20,0.4)
+x2,y2 = hagoDosCifras(xlist,ylist,30,0.05)
 
 from sklearn.model_selection import train_test_split
 
 xtrain,xtest,ytrain,ytest = train_test_split(x2,y2,test_size=0.2,random_state=42)
 
+xtrain,xtest,ytrain,ytest = xtrain/255,xtest/255,ytrain,ytest
+
 from sklearn.neighbors import KNeighborsClassifier
-knn_clf = KNeighborsClassifier()
+knn_clf = KNeighborsClassifier(n_neighbors=39,p=2,weights='distance')
 knn_clf.fit(xtrain,ytrain)
+
+import time
+inicio=time.time()
+knn_clf.predict(xtest[7].reshape(1,-1))
+fin=time.time()
+print(fin-inicio)
+#0.30017
+#Probar el modelo
+from sklearn.model_selection import cross_val_predict
+
+y_train_pred = cross_val_predict(knn_clf,xtrain,ytrain,cv=3)
+
+from sklearn.metrics import confusion_matrix,f1_score,accuracy_score
+
+y_predict = np.zeros((1000,11))
+for i in range(1000):
+    y_predict[i,:] = knn_clf.predict(xtest[i].reshape(1,-1))
+
+
+f1_score(ytest[:1000],y_predict,average='samples')
+#0.936633
+accuracy_score(ytest[:1000],y_predict,normalize=False)
+#0.835
+
+
+
+#Otro modelo
+
+knn_clf2 = KNeighborsClassifier(n_neighbors=39,p=2)
+knn_clf2.fit(xtrain,ytrain)
+
+import time
+inicio=time.time()
+knn_clf2.predict(xtest[7].reshape(1,-1))
+fin=time.time()
+print(fin-inicio)
+#0.2858
+
+y_predict2 = np.zeros((1000,11))
+for i in range(1000):
+    y_predict2[i,:] = knn_clf2.predict(xtest[i].reshape(1,-1))
+    
+
+f1_score(ytest[:1000],y_predict2,average='samples')
+#0.8718333
+accuracy_score(ytest[:1000],y_predict2,normalize=False)
+#0.644
+
+
+#Image
+
+img2 = cv2.resize(img,(28,56),interpolation=cv2.INTER_AREA)
+img2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+norm_img = np.zeros((10,10))
+(thresh, blackAndWhiteImage) = cv2.threshold(img2, 100, 255, cv2.THRESH_BINARY)
+th3 = cv2.adaptiveThreshold(img2,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,3,2)
+final_img = cv2.normalize(img2, norm_img,0,255, cv2.NORM_MINMAX)
+
+cv2.imshow('cosita',img2)
+cv2.imwrite("C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robotica/prueba2.png",th3)
+cv2.waitKey(0)
+
+knn_clf.predict(th3.reshape(1,-1))
+
+
+captura = cv2.VideoCapture('C:/Users/pc/OneDrive - Universidad de los andes/Septimo semestre/Robotica/96.mp4')
+while (captura.isOpened()):
+  contador = 59
+  ret, imagen = captura.read()
+  if ret == True:
+    contador = contador + 1
+    if contador == 60:
+        #imagen2 = cv2.flip(imagen,-1)
+        imagen2 = cv2.resize(imagen,(56,28),interpolation=cv2.INTER_AREA)
+        imagen2 = cv2.cvtColor(imagen2,cv2.COLOR_BGR2GRAY)
+        imagen2 = cv2.adaptiveThreshold(imagen2,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,3,2)
+        txt = knn_clf.predict(imagen2.reshape(1,-1))
+        cv2.putText(imagen,str(txt),(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,255),2,cv2.LINE_AA)
+        contador = 0
+    cv2.imshow('video', imagen2)
+    if cv2.waitKey(30) == ord('s'):
+      break
+  else: break
+captura.release()
+cv2.destroyAllWindows()
